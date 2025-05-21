f815d69d1d7612935eb61b2af953c68147af61b4
Julien WITTOUCK <julien.wittouck@gmail.com>
2025-05-21 19:21:46 +0200

    feat: update cloudnord data

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
 const config = {
   edition: 2025,
-  editionNumber: 8,
-  date: "12 et 13 Juin",
-  formattedStartDate: "2025-06-12",
-  formattedEndDate: "2025-06-13",
+  editionNumber: 6,
+  date: "23 Octobre",
+  formattedStartDate: "2025-10-23",
+  formattedEndDate: "2025-10-23",
   billetwebUrl: "https://www.billetweb.fr/devlille-",
   earlyBirds:
     "https://docs.google.com/forms/d/e/1FAIpQLSc-PIBQSB_G1WBTxeQIK2LUNm-GvaQYq08U9g4iGZCBQ-N-qw/viewform",
@@ -11,14 +11,14 @@ const config = {
   cmsUrl: "https://partenaire.devlille.fr/#",
   cms4partnersApi:
     "https://confily-486924521070.europe-west1.run.app/events/devlille-",
-  cfpStartedDate: "6 Janvier",
-  cfpEndedDate: "31 Mars",
+  cfpStartedDate: "26 Mai",
+  cfpEndedDate: "30 Juin",
   partershipUrl:
     "https://docs.google.com/presentation/d/e/2PACX-1vSo0McftZg-uPKKsoJnKGmzny9pEB2G_6XHVPK3MXiEfMk2J4UgXGJ_JWS77namY9lw5Fqj1Ace8Ftr/pub?start=false&loop=false&delayms=3000",
-  numberOfPeople: 1500,
-  numberOfSpeaker: 60,
-  numberOfTalks: 44,
-  numberOfTraks: 4,
+  numberOfPeople: 200,
+  numberOfSpeaker: 15,
+  numberOfTalks: 15,
+  numberOfTraks: 2,
   enabled: {
     welovedevs: false,
     billetweb: true,
